6380ee95e5829919fc4828c1e5090da3dadbddc0
Matt Zeunert <matt@mostlystatic.com>
2016-03-26 15:04:37 +0000

    Initially show message to remind users to enable custom formatters.

--- extension/devtool.js
+++ extension/devtool.js
@@ -1,3 +1,14 @@
+var timesExtensionHasLoaded = getAndIncrementTimesExtensionHasLoaded();
+
+// I don't think there's an easy way to detect if custom formatters have been enabled in the settings...
+var moreTimes = 4 - (timesExtensionHasLoaded - 1);
+if (moreTimes >= 0) {
+    var message = "Immutable Object Formatter: Make sure to check 'Enable custom formatters' in the DevTools settings.";
+    message += " (This message will show " + moreTimes + " more time" + (moreTimes === 1 ? "" :"s") + ".)";
+    chrome.devtools.inspectedWindow.eval( 
+      'console.log("' + message +  '")'
+      , function(){})
+}
 function loadDevToolsFormatters(){
     fetch("bundle.js").then(function(response){
         response.text().then(function(text){
@@ -14,3 +25,14 @@ function loadDevToolsFormatters(){
 loadDevToolsFormatters();
 // Reload if page is refreshed
 chrome.devtools.network.onNavigated.addListener(loadDevToolsFormatters)
+
+
+function getAndIncrementTimesExtensionHasLoaded(){
+  var timesExtensionHasLoaded = localStorage.getItem("timesExtensionHasLoaded");
+  if (timesExtensionHasLoaded === undefined){
+    timesExtensionHasLoaded = 0;
+  }
+  timesExtensionHasLoaded++;
+  localStorage.setItem("timesExtensionHasLoaded", timesExtensionHasLoaded);
+  return parseInt(timesExtensionHasLoaded, 10)
+  }
